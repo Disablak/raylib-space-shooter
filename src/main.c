6183a713c6467ad0ef068f6a5a767aa319190f46
Max Verchynskyi <disablaksteam@gmail.com>
2025-01-20 14:00:31 +0200

    ship movement and physics

--- src/main.c
+++ src/main.c
@@ -1,71 +1,129 @@
-/*
-Raylib example file.
-This is an example main file for a simple raylib project.
-Use this as a starting point or replace it with your code.
-
-For a C++ project simply rename the file to .cpp and re-run the build script 
-
--- Copyright (c) 2020-2024 Jeffery Myers
---
---This software is provided "as-is", without any express or implied warranty. In no event 
---will the authors be held liable for any damages arising from the use of this software.
-
---Permission is granted to anyone to use this software for any purpose, including commercial 
---applications, and to alter it and redistribute it freely, subject to the following restrictions:
-
---  1. The origin of this software must not be misrepresented; you must not claim that you 
---  wrote the original software. If you use this software in a product, an acknowledgment 
---  in the product documentation would be appreciated but is not required.
---
---  2. Altered source versions must be plainly marked as such, and must not be misrepresented
---  as being the original software.
---
---  3. This notice may not be removed or altered from any source distribution.
-
-*/
-
+#include "stdio.h"
 #include "raylib.h"
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+#include "raymath.h"
 
+
+typedef struct Ship
+{
+	Vector2 pos;
+	Vector2 vel;
+	Vector2 move_dir;
+	float angle;
+	float angle_vel;
+	float speed;
+	float rot_speed;
+	float speed_acc;
+	float speed_max;
+	Rectangle dest;
+} Ship;
+
+float LerpAngle(float startAngle, float endAngle, float t);
+
+
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 800;
+
+Texture2D atlas;
+Ship player_ship;
+Rectangle ship_texture_rect = {0, 0, 32, 32};
+Vector2 draw_pos = {400, 200};
+Vector2 ship_origin = {16, 16};
+
+
+void UpdateShipPos(Ship *ship)
+{
+	ship->pos = Vector2Add(ship->pos, Vector2Scale(ship->vel, GetFrameTime()));
+    ship->angle += ship->angle_vel * GetFrameTime();
+
+	if (ship->pos.x > SCREEN_WIDTH) ship->pos.x = 0;
+    if (ship->pos.x < 0) ship->pos.x = SCREEN_WIDTH;
+    if (ship->pos.y > SCREEN_HEIGHT) ship->pos.y = 0;
+    if (ship->pos.y < 0) ship->pos.y = SCREEN_HEIGHT;
+}
+
+void ApplyThrust(Ship *ship)
+{
+	float thrust = ship->speed_acc * GetFrameTime();
+    Vector2 force = { cosf(ship->angle * DEG2RAD) * thrust, sinf(ship->angle * DEG2RAD) * thrust };
+    ship->vel = Vector2Add(ship->vel, force);
+}
+
+void GameInit()
+{
+	atlas = LoadTexture("atlas.png");
+
+	player_ship.dest = (Rectangle){SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 32, 32};
+	player_ship.pos = (Vector2){SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2};
+	player_ship.angle = 0;
+	player_ship.rot_speed = 100;
+	player_ship.speed_acc = 200;
+}
+
+void DrawShip(Ship *ship, Texture2D texture)
+{
+	ship->dest.x = ship->pos.x;
+	ship->dest.y = ship->pos.y;
+	DrawTexturePro(texture, ship_texture_rect, ship->dest, ship_origin, ship->angle, WHITE);
+}
+
+void ListenInputs()
+{
+    if (IsKeyDown(KEY_UP))    ApplyThrust(&player_ship);
+    if (IsKeyDown(KEY_LEFT))  player_ship.angle_vel = -player_ship.rot_speed;
+	if (IsKeyDown(KEY_RIGHT)) player_ship.angle_vel = player_ship.rot_speed;
+	//player_ship.angle += player_ship.rot_speed * GetFrameTime();
+	//player_ship.angle -= player_ship.rot_speed * GetFrameTime();
+	//player_ship.speed += player_ship.speed_acc * GetFrameTime();
+    //if (IsKeyDown(KEY_DOWN))  player_ship.speed -= player_ship.speed_acc * GetFrameTime();
+
+	if (IsKeyReleased(KEY_LEFT) || IsKeyReleased(KEY_RIGHT)) player_ship.angle_vel = 0;
+
+	//if (IsKeyDown(KEY_UP)) UpdatePlayerMoveDir();
+
+	//player_ship.speed = Clamp(player_ship.speed, 1, player_ship.speed_max);
+
+	UpdateShipPos(&player_ship);
+}
+
+void DrawScreen()
+{
+	BeginDrawing();
+
+		ClearBackground(BLACK);
+
+		DrawShip(&player_ship, atlas);
+		
+	EndDrawing();
+}
+
+void Cleanup()
+{
+	UnloadTexture(atlas);
+}
 
 int main ()
 {
-	// Tell the window to use vsync and work on high DPI displays
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
-
-	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Hello Raylib");
-
-	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
+	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Space Shooter");
 	SearchAndSetResourceDir("resources");
+	SetTargetFPS(60);
 
-	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
+	GameInit();
 	
-	// game loop
-	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
+	while (!WindowShouldClose())
 	{
-		// drawing
-		BeginDrawing();
-
-		// Setup the back buffer for drawing (clear color and depth buffers)
-		ClearBackground(BLACK);
-
-		// draw some text using the default font
-		DrawText("Hello Disa", 200,200,20,WHITE);
-
-		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
-		
-		// end the frame and get ready for the next one  (display frame, poll input, etc...)
-		EndDrawing();
+		ListenInputs();
+		DrawScreen();
 	}
 
-	// cleanup
-	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
+	Cleanup();
 
-	// destroy the window and cleanup the OpenGL context
 	CloseWindow();
 	return 0;
+}
+
+float LerpAngle(float startAngle, float endAngle, float t) {
+	float difference = fmod(endAngle - startAngle + 180, 360) - 180;
+	return startAngle + difference * t;
 }
