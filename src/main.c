24ee6c03573083907d585a2e2942b832705dac94
Max Verchynskyi <disablaksteam@gmail.com>
2025-01-22 13:17:26 +0200

    bullet shoot

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "stdio.h"
+#include <stdlib.h> 
 #include "raylib.h"
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
 #include "raymath.h"
@@ -18,6 +19,15 @@ typedef struct Ship
 	float speed_max;
 	Rectangle dest;
 } Ship;
+
+typedef struct Bullet
+{
+	bool is_active;
+	Vector2 pos;
+	float angle;
+	float speed;
+} Bullet;
+
 
 float LerpAngle(float startAngle, float endAngle, float t);
 
@@ -40,6 +50,11 @@ Rectangle black_hole_texture_rect = {64, 0, 32, 32};
 Rectangle black_hole_dest = {SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 128, 128};
 Vector2 black_hole_origin = {64, 64};
 
+const int BULLETS_LENGTH = 100;
+Bullet all_bullets[BULLETS_LENGTH];
+Rectangle bullet_texture_rect = {96, 0, 32, 32};
+Rectangle bullet_dest = {0, 0, 32, 32};
+
 
 void GameInit()
 {
@@ -51,6 +66,13 @@ void GameInit()
 	player_ship.angle = 0;
 	player_ship.rot_speed = 100;
 	player_ship.speed_acc = 200;
+
+	for (size_t i = 0; i < BULLETS_LENGTH; i++)
+	{
+		all_bullets[i].is_active = false;
+		all_bullets[i].angle = 0;
+		all_bullets[i].speed = 1000;
+	}
 }
 
 void DrawThrustEffect(Ship *ship)
@@ -81,11 +103,42 @@ void DrawBlackHole()
 	DrawTexturePro(atlas, black_hole_texture_rect, black_hole_dest, black_hole_origin, GetTime() * 50, WHITE);
 }
 
+void DrawBullets()
+{
+	for (size_t i = 0; i < BULLETS_LENGTH; i++)
+	{
+		Bullet bullet = all_bullets[i];
+		if (bullet.is_active == false) 
+			continue;
+		
+		bullet_dest.x = bullet.pos.x;
+		bullet_dest.y = bullet.pos.y;
+		DrawTexturePro(atlas, bullet_texture_rect, bullet_dest, ship_origin, bullet.angle, WHITE);
+	}
+}
+
 void ApplyThrust(Ship *ship)
 {
 	float thrust = ship->speed_acc * GetFrameTime();
     Vector2 force = { cosf(ship->angle * DEG2RAD) * thrust, sinf(ship->angle * DEG2RAD) * thrust };
     ship->vel = Vector2Add(ship->vel, force);
+}
+
+void Shoot()
+{
+	if (player_ship.is_alive == false)
+		return;
+
+	for (size_t i = 0; i < BULLETS_LENGTH; i++)
+	{
+		if (all_bullets[i].is_active == false) 
+		{
+            all_bullets[i].is_active = true;
+            all_bullets[i].pos = player_ship.pos;
+            all_bullets[i].angle = player_ship.angle;
+            break;
+		}
+	}
 }
 
 void ListenInputs()
@@ -95,6 +148,8 @@ void ListenInputs()
 	if (IsKeyDown(KEY_RIGHT)) player_ship.angle_vel = player_ship.rot_speed;
 
 	if (IsKeyReleased(KEY_LEFT) || IsKeyReleased(KEY_RIGHT)) player_ship.angle_vel = 0;
+
+	if (IsKeyDown(KEY_SPACE)) Shoot();
 }
 
 void UpdateBlackHoleInfluence()
@@ -138,10 +193,28 @@ void CheckCollision()
 		DestroyPlayerShip();
 }
 
+void UpdateBullets()
+{
+	for (size_t i = 0; i < BULLETS_LENGTH; i++)
+	{
+		if (all_bullets[i].is_active == false)
+			continue;
+		
+		Vector2 dir = {cosf(all_bullets[i].angle * DEG2RAD), sinf(all_bullets[i].angle * DEG2RAD)};
+		Vector2 vel = Vector2Scale(dir, all_bullets[i].speed * GetFrameTime());
+		Vector2 new_pos = Vector2Add(all_bullets[i].pos, vel);
+		all_bullets[i].pos = new_pos;
+
+		if (new_pos.x > SCREEN_WIDTH || new_pos.x < 0 || new_pos.y > SCREEN_HEIGHT || new_pos.y < 0)
+			all_bullets[i].is_active = false;
+	}
+}
+
 void GameLogic()
 {
 	UpdateBlackHoleInfluence();
 	UpdateShipPos(&player_ship);
+	UpdateBullets();
 	CheckCollision();
 }
 
@@ -153,6 +226,7 @@ void DrawScreen()
 
 		DrawShip(&player_ship);
 		DrawBlackHole();
+		DrawBullets();
 		
 	EndDrawing();
 }
